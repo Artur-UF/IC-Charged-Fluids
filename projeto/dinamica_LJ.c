763d99157f9cc6b46a5165f1ffbd9531930a8246
Artur Uhlik Frohlich <artur.uhlik@gmail.com>
2022-11-11 15:18:04 -0300

    Atualização

--- projeto/dinamica_LJ.c
+++ projeto/dinamica_LJ.c
@@ -1,9 +1,14 @@
 /*
 Compilar: $ gcc dinamica_LJ.c func.c -o dinamica_LJ -lm
+Para abrir o Jmol e verificar $ java -jar Jmol.jar
 
 Esse código realiza a dinâmica das partículas usando o potencial de Lennard-Jones
 e salva um arquivo binario a cada 10 passos contendo todas as características de cada partícula
 no determinado instante.
+
+10k até equilibrio
+salva 500 e faz o video	
+
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +24,7 @@ no determinado instante.
 #define R 1.			// Raio das partículas
 #define DT 0.005		// Delta de tempo
 #define FRIC 0.1		// Coeficiente de Fricção
-#define TF 10.			// Tempo final
+#define TF 53.			// Tempo final
 #define CHECKPOINT 10.	// Número de passos pulados
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 
@@ -79,6 +84,12 @@ int main (){
 	sprintf(arkinfo, "LJ_L%.1lf_TF%.1lf/info.txt", L, TF);
 	FILE *infos = fopen(arkinfo, "w");
 	
+	// Aqui cria o arquivo que será usado para a animação
+	char passo[25];
+	sprintf(passo, "LJ_L%.1lf_TF%.1lf/anim.txt", L, TF);
+	
+	FILE *in = fopen(passo, "w");
+	
 	//Loop Temporal
 	double con, c0, c1, c2, desv_r, desv_v, cvv, cvv2, dt2, xold, yold, zold;
 	con = FRIC*DT;
@@ -91,7 +102,9 @@ int main (){
 	cvv2 = sqrt(1 - (cvv*cvv));
 	dt2 = DT * DT;
 	
+	// Contadores auxiliares para o loop
 	int counter = 0;
+	int contanim = 0;
 	for (double t = 0; t <= TF; t += DT){
 		// Cálculo inicial das forças
 		forcas(todas, n, lx, ly, lz);
@@ -137,6 +150,7 @@ int main (){
 			c2 * DT * todas[j].f[2] + desv_v * (cvv * todas[j].gaussian[2] + cvv2 * gausran());
 		}
 		
+		/* Esse salva em binário
 		// Escrevendo arquivo do passo
 		if (fmod(counter, CHECKPOINT) == 0){
 			char passo[25];
@@ -147,6 +161,23 @@ int main (){
 			fwrite(todas, sizeof(particle), n, in);
 			fclose(in);
 		}
+		*/
+		
+		// Esse salva em .txt pra fazer a animação
+		if (counter >= 10000){
+			fprintf(in, "%d\n\n", n);
+			for (int i = 0; i < n; ++i){
+				if (i < n/2){
+					fprintf(in, "Na ");
+				}else{
+					fprintf(in, "Cla ");
+				}
+				fprintf(in, "%lf %lf %lf\n", todas[i].p[0], todas[i].p[1], todas[i].p[2]);
+			}
+			fprintf(in, "\n");
+			++contanim;
+			if (contanim >= 500) break;
+		}
 		++counter;
 	}
 
@@ -154,10 +185,11 @@ int main (){
 	clock_t toc = clock();
 	double time = (double)(toc - tic)/CLOCKS_PER_SEC;	
 	
-	fprintf(infos, "N = %d\nR = %.2lf\nL = %.1lf\nDT = %lf\nTF = %.2lf\nFRIC = %.2lf\nPassos = %d\nTempo de execução = %lfs\n"
-			, n, R, L, DT, TF, FRIC, counter, time);
+	fprintf(infos, "N = %d\nR = %.2lf\nL = %.1lf\nDT = %lf\nTF = %.2lf\nFRIC = %.2lf\nPassos = %d\nTempo de execução = %lfs\n10000 passos até o equilíbrio."
+			, n, R, L, DT, TF, FRIC, contanim, time);
 	
 	fclose(ark);
+	fclose(in);	
 	fclose(infos);
 	free(todas);
 	return 0;

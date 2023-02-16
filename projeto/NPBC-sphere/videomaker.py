55ffd42e6b9d227f95047d08e66e8d8e1439860d
Artur Uhlik Frohlich <artur.uhlik@gmail.com>
2023-02-15 21:51:51 -0300

    Atualização

---
+++ projeto/NPBC-sphere/videomaker.py
@@ -0,0 +1,24 @@
+import imageio
+import imageio.v3 as iio
+from pathlib import Path
+import os
+
+images = list()
+
+path0 = os.path.join(os.getcwd(), f'teste')
+
+for file in Path(path0).iterdir():
+    if not file.is_file():
+        continue
+    images.append(iio.imread(file))
+
+path1 = os.path.join(os.getcwd(), f'movie-ic.gif')
+
+'''
+with imageio.get_writer(path1, mode='I') as writer:
+    for filename in images:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+'''
+
+imageio.mimsave(path1, images, fps=2)
